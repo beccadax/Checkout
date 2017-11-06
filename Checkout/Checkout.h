a9b528ef886134f654fee36be063b3af3b9a1851
Brent Royal-Gordon <brent@architechies.com>
2017-11-05 16:36:12 -0800

    Initial Commit

---
+++ Checkout/Checkout.h
@@ -0,0 +1,19 @@
+//
+//  Checkout.h
+//  Checkout
+//
+//  Created by Brent Royal-Gordon on 11/5/17.
+//  Copyright © 2017 Architechies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Checkout.
+FOUNDATION_EXPORT double CheckoutVersionNumber;
+
+//! Project version string for Checkout.
+FOUNDATION_EXPORT const unsigned char CheckoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Checkout/PublicHeader.h>
+
+
