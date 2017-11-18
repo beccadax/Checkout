1171fd88949e189887ffc1cc481af440817870f6
Brent Royal-Gordon <brent@architechies.com>
2017-11-18 02:09:39 -0800

    Add type-erasing protocol to Fallible

--- Checkout Viewer/Fallible.swift
+++ Checkout Viewer/Fallible.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum Fallible<Value> {
+enum Fallible<Value>: AnyFallible {
     case success(Value)
     case failure(Error)
     
@@ -18,6 +18,42 @@ enum Fallible<Value> {
         }
         catch {
             self = .failure(error)
+        }
+    }
+    
+    var value: Value? {
+        get {
+            guard case .success(let value) = self else { return nil }
+            return value
+        }
+    }
+    
+    var error: Error? {
+        get {
+            guard case .failure(let error) = self else { return nil }
+            return error
+        }
+    }
+    
+    var _anyValue: Any? {
+        return value
+    }
+}
+
+protocol AnyFallible {
+    var _anyValue: Any? { get }
+    var error: Error? { get }
+}
+
+extension Fallible where Value == Any {
+    init(erased anyFallible: AnyFallible) {
+        switch (anyFallible._anyValue, anyFallible.error) {
+        case (let value?, nil):
+            self = .success(value)
+        case (nil, let error?):
+            self = .failure(error)
+        default:
+            fatalError("Invalid AnyFallible conformance")
         }
     }
 }
